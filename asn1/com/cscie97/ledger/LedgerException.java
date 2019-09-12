23f5e10d4741b8eccd82bb4bfa6ef31cacaa4839
Matt Thomas <matthew_thomas@hms.harvard.edu>
2019-09-11 20:35:01 -0400

    Copied CPExecption to Ledger

--- asn1/com/cscie97/ledger/LedgerException.java
+++ asn1/com/cscie97/ledger/LedgerException.java
@@ -1,7 +1,17 @@
 package com.cscie97.ledger;
 
-public class LedgerException {
+public class LedgerException extends Exception {
     private String action;
     private String reason;
 
+    public LedgerException() {
+        super();
+    }
+
+    public LedgerException(String message, String reason) {
+        super(message);
+        this.action = message;
+        this.reason = reason;
+        System.out.println("LedgerException....");
+    }
 }
