8f2fb2d4b49aa994ea0fbd5a595acd0ed3e94c3e
Matt Thomas <matthew_thomas@hms.harvard.edu>
2019-09-11 20:35:22 -0400

    Add constructor to transaction, toString

--- asn1/com/cscie97/ledger/Transaction.java
+++ asn1/com/cscie97/ledger/Transaction.java
@@ -1,13 +1,28 @@
 package com.cscie97.ledger;
 
 public class Transaction {
+    // Properties
     private String transactionId;
-    private Integer amount;
-    private Integer fee;
+    private int amount;
+    private int fee;
     private String payload;
 
-    public Transaction(Integer amount, Integer fee) {
+    // Associations
+    private Account payer;
+    private Account receiver;
+
+    public Transaction(String id, int amount, int fee, String payload, Account payer, Account receiver) {
+        this.transactionId = id;
         this.amount = amount;
         this.fee = fee;
+        this.payload = payload;
+        this.payer = payer;
+        this.receiver = receiver;
+    }
+
+
+
+    public String toString() {
+        return "Transaction " + this.transactionId + " for amount " + this.amount + " and payload of " + this.payload;
     }
 }
